5e640bf3b16008d24173475c25c0643f5056b7dd
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-08-09 15:50:51 +0200

    changes

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -10,13 +10,14 @@ describe('App', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
+    fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
   });
