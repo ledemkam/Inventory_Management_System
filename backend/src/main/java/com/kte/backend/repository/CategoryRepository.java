0cd3066a5b2cf6db78db69c853af76ebc2140ad8
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-08-11 20:14:19 +0200

    feat: add case-insensitive find methods for Category, Product, and Supplier repositories

--- backend/src/main/java/com/kte/backend/repository/CategoryRepository.java
+++ backend/src/main/java/com/kte/backend/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.kte.backend.repository;
 import com.kte.backend.models.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, String> {
+    Optional<Category> findByNameIgnoreCase(String name);
 }
--- backend/src/main/java/com/kte/backend/repository/SupplierRepository.java
+++ backend/src/main/java/com/kte/backend/repository/SupplierRepository.java
@@ -3,5 +3,9 @@ package com.kte.backend.repository;
 import com.kte.backend.models.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SupplierRepository extends JpaRepository<Supplier, String> {
+    Optional<Supplier> findByNameIgnoreCase(String name);
+
 }
