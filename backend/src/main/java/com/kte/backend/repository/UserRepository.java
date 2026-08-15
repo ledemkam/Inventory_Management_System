b6959a4e49cceff75dab67247f5efa00fa450d30
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-08-15 20:23:40 +0200

    feat: implement UserService with user registration and username existence check

--- backend/src/main/java/com/kte/backend/repository/UserRepository.java
+++ backend/src/main/java/com/kte/backend/repository/UserRepository.java
@@ -4,8 +4,8 @@ package com.kte.backend.repository;
 import com.kte.backend.models.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findByEmail(String email);
+    boolean findByEmail(String email);
+
+    boolean existsByUsername(String username);
 }
