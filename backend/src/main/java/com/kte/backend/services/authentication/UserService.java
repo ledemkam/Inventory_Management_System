92d50664eddfe1b6af8f9e3564ff252f9ab30215
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-08-15 13:57:28 +0200

    feat: define UserService interface with user management methods

--- backend/src/main/java/com/kte/backend/services/authentication/UserService.java
+++ backend/src/main/java/com/kte/backend/services/authentication/UserService.java
@@ -1,4 +1,23 @@
 package com.kte.backend.services.authentication;
 
-public class UserService {
+import com.kte.backend.models.dto.request.LoginRequest;
+import com.kte.backend.models.dto.request.RegisterRequest;
+import com.kte.backend.models.dto.request.UserRequest;
+import com.kte.backend.models.dto.response.UserResponse;
+import com.kte.backend.models.entity.User;
+
+public interface UserService {
+    UserResponse registerUser(final RegisterRequest registerRequest);
+
+    UserResponse loginUser(final LoginRequest loginRequest);
+
+    UserResponse getAllUsers();
+
+    User getCurrentLoggedInUser();
+
+    UserResponse updateUser(final String id, final UserRequest userRequest);
+
+    UserResponse deleteUser(final String id);
+
+    UserResponse getUserTransactions(final String id);
 }
