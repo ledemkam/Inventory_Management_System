3fd22f61542a570fbeba3d73a79ec18257f9c931
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-08-16 18:05:13 +0200

    feat(authentication): add AuthenticationController and UIAuthenticationController interface for user registration

---
+++ backend/src/main/java/com/kte/backend/controllers/UIAuthenticationController.java
@@ -0,0 +1,34 @@
+package com.kte.backend.controllers;
+
+
+import com.kte.backend.models.dto.request.LoginRequest;
+import com.kte.backend.models.dto.request.RegisterRequest;
+import com.kte.backend.models.dto.response.LoginResponse;
+import com.kte.backend.models.dto.response.UserResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "UI Authentication Controllers", description = "Endpoints for Login and Registration")
+public interface UIAuthenticationController {
+
+    @Operation(summary = "Registration Endpoint", description = "Endpoint for user registration")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content(schema = @Schema(implementation = Error.class))),
+            @ApiResponse(responseCode = "409", description = "Conflict - User already exists",
+                    content = @Content(schema = @Schema(implementation = Error.class))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error",
+                    content = @Content(schema = @Schema(implementation = Error.class))),
+    })
+    ResponseEntity<UserResponse> registerUser(@Valid @RequestBody final RegisterRequest registerRequest);
+
+
+}
