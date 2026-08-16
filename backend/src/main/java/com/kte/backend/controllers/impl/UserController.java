139ca071eb73ab1768ebc5078dc0e6f3e8f466d9
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-08-16 19:16:40 +0200

    feat(user): add updateUser method to UIUserController and UserController for user updates

--- backend/src/main/java/com/kte/backend/controllers/impl/UserController.java
+++ backend/src/main/java/com/kte/backend/controllers/impl/UserController.java
@@ -2,6 +2,7 @@ package com.kte.backend.controllers.impl;
 
 import com.kte.backend.common.PageResponse;
 import com.kte.backend.controllers.UIUserController;
+import com.kte.backend.models.dto.request.UserRequest;
 import com.kte.backend.models.dto.response.UserResponse;
 import com.kte.backend.services.authentication.UserService;
 import lombok.RequiredArgsConstructor;
@@ -29,5 +30,12 @@ public class UserController implements UIUserController {
         log.debug("Received request to get all users with pageable: {}", pageable);
         final PageResponse<UserResponse> users = userService.getAllUsers(pageable);
         return ResponseEntity.ok(users);
+    }
+
+    @Override
+    public ResponseEntity<UserResponse> updateUser(String id, UserRequest request) {
+        log.debug("Received request to update user with id: {} and request: {}", id, request);
+        final UserResponse updatedUser = userService.updateUser(id, request);
+        return ResponseEntity.accepted().body(updatedUser);
     }
 }
