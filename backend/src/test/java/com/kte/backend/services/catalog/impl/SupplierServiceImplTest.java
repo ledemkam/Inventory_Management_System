014a60896b1c55a14f9104ff5a9911040b5bcfe1
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-08-23 10:40:44 +0200

    feat(supplier): add unit tests for create method in SupplierServiceImpl with SupplierValidator mock

--- backend/src/test/java/com/kte/backend/services/catalog/impl/SupplierServiceImplTest.java
+++ backend/src/test/java/com/kte/backend/services/catalog/impl/SupplierServiceImplTest.java
@@ -5,6 +5,7 @@ import com.kte.backend.models.dto.request.SupplierRequest;
 import com.kte.backend.models.dto.response.SupplierResponse;
 import com.kte.backend.models.entity.Supplier;
 import com.kte.backend.repository.SupplierRepository;
+import com.kte.backend.validator.SupplierValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @ExtendWith({SpringExtension.class, MockitoExtension.class})
 @DisplayName("SupplierServiceImpl Unit Tests")
@@ -25,6 +29,9 @@ class SupplierServiceImplTest {
 
     @Mock
     private SupplierRepository supplierRepository;
+
+    @Mock
+    private SupplierValidator supplierValidator;
 
     @InjectMocks
     private SupplierServiceImpl supplierService;
@@ -54,5 +61,16 @@ class SupplierServiceImplTest {
     @Test
     @DisplayName("should create supplier when it does not exist")
     void should_create_supplier_when_exist() {
+        //Given
+        when(supplierMapper.dtoToEntity(supplierRequest)).thenReturn(supplier);
+        when(supplierRepository.save(supplier)).thenReturn(supplier);
+        when(supplierMapper.entityToDto(supplier)).thenReturn(supplierResponse);
+        doNothing().when(supplierValidator).checkSupplierAlreadyExistsByName(supplierRequest.name());
+
+        //When
+        SupplierResponse response = supplierService.create(supplierRequest);
+
+        //Then
+        assertThat(response).isNotNull();
     }
 }
