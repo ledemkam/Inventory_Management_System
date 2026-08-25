baa29a727da10cb94832d7e663b37c57d3261118
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-08-25 20:52:21 +0200

    feat(supplier): add unit test for retrieving all suppliers in SupplierControllerTest

--- backend/src/test/java/com/kte/backend/controllers/impl/SupplierControllerTest.java
+++ backend/src/test/java/com/kte/backend/controllers/impl/SupplierControllerTest.java
@@ -1,8 +1,10 @@
 package com.kte.backend.controllers.impl;
 
+import com.kte.backend.common.PageResponse;
 import com.kte.backend.config.SecurityConfig;
 import com.kte.backend.mapper.SupplierMapper;
 import com.kte.backend.models.dto.request.SupplierRequest;
+import com.kte.backend.models.dto.response.CategoryResponse;
 import com.kte.backend.models.dto.response.SupplierResponse;
 import com.kte.backend.models.entity.Supplier;
 import com.kte.backend.security.JwtTokenService;
@@ -21,12 +23,15 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -119,5 +124,32 @@ class SupplierControllerTest {
                 .andExpect(jsonPath("$.id", is(supplierId)))
                 .andExpect(jsonPath("$.name", is("updated_Supplier_A")))
                 .andExpect(jsonPath("$.address", is("124 Main St")));
+    }
+
+    @Test
+    @DisplayName("should return all suppliers")
+    void should_Return_All_Suppliers() throws Exception {
+        //GIVEN
+        final PageResponse<SupplierResponse> supplierResponsePageResponse = PageResponse.<SupplierResponse>builder()
+                .content(List.of(supplierResponse))
+                .page(0)
+                .size(10)
+                .totalElements(1)
+                .totalPages(1)
+                .hasNext(false)
+                .hasPrevious(false)
+                .isFirst(true)
+                .isLast(true)
+                .build();
+        when(supplierService.findAll(any())).thenReturn(supplierResponsePageResponse);
+        //WHEN & THEN
+        mockMvc.perform(get("/api/v1/suppliers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(supplierResponsePageResponse)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(1)))
+                .andExpect(jsonPath("$.content[0].id", is("1")))
+                .andExpect(jsonPath("$.content[0].name", is("Supplier_A")))
+                .andExpect(status().isOk());
     }
 }
