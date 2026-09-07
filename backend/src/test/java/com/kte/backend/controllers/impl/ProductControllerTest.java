02fc3bb1286101ec78c3020f57022d79066bad08
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-09-07 21:30:34 +0200

    test(product): refactor ProductControllerTest to use RestTestClient instead of MockMvc

--- backend/src/test/java/com/kte/backend/controllers/impl/ProductControllerTest.java
+++ backend/src/test/java/com/kte/backend/controllers/impl/ProductControllerTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
@@ -38,7 +37,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductController.class)
-@AutoConfigureMockMvc
 @AutoConfigureRestTestClient
 @Import(SecurityConfig.class)
 @DisplayName("ProductController Test")
