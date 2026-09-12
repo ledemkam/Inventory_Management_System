2df0bda4f1ebedf797af658a371eed84c2039a57
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-09-12 11:27:06 +0200

    feat(controller): implement purchase endpoint in TransactionController for transaction recording

--- backend/src/main/java/com/kte/backend/controllers/impl/TransactionController.java
+++ backend/src/main/java/com/kte/backend/controllers/impl/TransactionController.java
@@ -5,10 +5,15 @@ import com.kte.backend.controllers.UITransactionController;
 import com.kte.backend.models.dto.request.TransactionRequest;
 import com.kte.backend.models.dto.response.TransactionResponse;
 import com.kte.backend.models.enums.TransactionStatus;
+import com.kte.backend.services.catalog.TransactionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +23,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/api/v1/transactions")
 public class TransactionController implements UITransactionController {
+    private final TransactionService transactionService;
+
     @Override
-    public ResponseEntity<TransactionResponse> purchase(TransactionRequest request) {
-        return null;
+    @PostMapping("/purchase")
+    public ResponseEntity<TransactionResponse> purchase(
+            @Valid
+            @RequestBody final TransactionRequest request) {
+        log.info("Received request to record a purchase transaction: {}", request);
+        TransactionResponse newTransaction = transactionService.restockInventory(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTransaction);
     }
 
     @Override
