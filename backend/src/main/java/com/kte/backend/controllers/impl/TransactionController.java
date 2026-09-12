a51386f9a10d561a6497864082cfc99765f78f74
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-09-12 11:37:37 +0200

    feat(controller): implement getAllTransactions endpoint in TransactionController for retrieving all transactions

--- backend/src/main/java/com/kte/backend/controllers/impl/TransactionController.java
+++ backend/src/main/java/com/kte/backend/controllers/impl/TransactionController.java
@@ -12,10 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -52,8 +49,11 @@ public class TransactionController implements UITransactionController {
     }
 
     @Override
-    public ResponseEntity<PageResponse<TransactionResponse>> getAllTransactions(Pageable pageable) {
-        return null;
+    @GetMapping
+    public ResponseEntity<PageResponse<TransactionResponse>> getAllTransactions(final Pageable pageable) {
+        PageResponse<TransactionResponse> response = transactionService.findAll(pageable);
+        log.debug("Received request to get all transactions with pageable: {}", pageable);
+        return ResponseEntity.ok(response);
     }
 
     @Override
